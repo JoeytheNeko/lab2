8e991c9f4acddcb5187d3536693569530b20f1ff
joeystevens <felisstevens@aol.com>
2019-05-02 13:30:50 -0700

    lab3 project

---
+++ Js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var userName = prompt('hi, what is your name? i am joey.');
+alert ('nice to meet you '+userName+' glad to have you in codefellows. lets play a guessing game.');
+console.log ('did first question and got username');
+var answerone = prompt('is my middle name henry? answer yes or no');
+if (answerone === 'yes') alert ('yes my middle name is henry');
+var answertwo = prompt('do i like chocolate? answer yes or no');
+if (answertwo === 'yes') alert ('yes I do like chocolate');
+var answerthree = prompt('do I enjoy video games? answer yes or no');
+if (answerthree === 'yes') alert('yes I enjoy video games greatly');
+var answerfour = prompt('do I enjoy pets? answer yes or no');
+if (answerfour === 'yes') alert ('yes I greatly enjoy the company of pets');
+var answerfive = prompt('do I enjoy dungeons and dragons? answer yes or no');
+if (answerfive === 'yes') alert ('yes, I never go anywhere without at least one of my D&D books');
